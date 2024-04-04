a2990a4ed03a49ed8d078078dca71e798ebbd916
Swx-GIX-Laptop <mhwu@uw.edu>
2024-04-04 16:33:03 -0700

    init

--- robot_api/src/robot_api/gripper.py
+++ robot_api/src/robot_api/gripper.py
@@ -1,36 +1,31 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
 import rospy
-import control_msgs.msg
 import actionlib
+from control_msgs.msg import GripperCommandAction, GripperCommandGoal  # Import necessary message types
 
-ACTION_NAME = '/gripper_controller/gripper_action'  # Modify as necessary
-CLOSED_POS = 0.0  # The position for a fully-closed gripper (meters)
-OPENED_POS = 0.10  # The position for a fully-open gripper (meters)
+ACTION_NAME = 'gripper_controller/gripper_action'  # The action server name for the gripper
+CLOSED_POS = 0.0  # The position for a fully-closed gripper (meters).
+OPENED_POS = 0.10  # The position for a fully-open gripper (meters).
 
 class Gripper(object):
     """Gripper controls the robot's gripper."""
-
     MIN_EFFORT = 35  # Min grasp force, in Newtons
     MAX_EFFORT = 100  # Max grasp force, in Newtons
 
     def __init__(self):
         # Create actionlib client
-        self.client = actionlib.SimpleActionClient(ACTION_NAME, control_msgs.msg.GripperCommandAction)
-        # Wait for the server
-        rospy.loginfo("Waiting for gripper action server...")
+        self.client = actionlib.SimpleActionClient(ACTION_NAME, GripperCommandAction)
+        # Wait for the action server to come up
         self.client.wait_for_server()
-        rospy.loginfo("Gripper action server found!")
 
     def open(self):
         """Opens the gripper."""
-        goal = control_msgs.msg.GripperCommandGoal()
-        goal.command.position = OPENED_POS  # Set to fully open
-        goal.command.max_effort = self.MIN_EFFORT  # Minimal effort
+        goal = GripperCommandGoal()
+        goal.command.position = OPENED_POS  # Set the position for the gripper to open
+        goal.command.max_effort = 0  # Set max effort to 0 for opening
         self.client.send_goal(goal)
-        rospy.loginfo("Opening gripper...")
         self.client.wait_for_result()
-        rospy.loginfo("Gripper opened.")
 
     def close(self, max_effort=MAX_EFFORT):
         """Closes the gripper.
@@ -39,11 +34,10 @@ class Gripper(object):
             max_effort: The maximum effort, in Newtons, to use. Note that this
                 should not be less than 35N, or else the gripper may not close.
         """
-        goal = control_msgs.msg.GripperCommandGoal()
-        goal.command.position = CLOSED_POS  # Set to fully closed
-        goal.command.max_effort = max_effort  # Use specified effort
+        goal = GripperCommandGoal()
+        goal.command.position = CLOSED_POS  # Set the position for the gripper to close
+        goal.command.max_effort = max_effort  # Set the maximum effort for closing
         self.client.send_goal(goal)
-        rospy.loginfo("Closing gripper...")
         self.client.wait_for_result()
-        rospy.loginfo("Gripper closed.")
+
 
